8be92eff840d1814f5e380ce5b78fa2dc562090b
Jon Kuperman <jon.kuperman@gmail.com>
2013-01-01 15:16:56 -0500

    added mobile navigation hide/show

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-$(window).scroll(function(){if($(window).width()>850){var e=window.pageYOffset;if(e<=0){var t=50+e*-1;$("header").animate({height:t},5,function(){$("h1.home").animate({lineHeight:t+"px"},5,function(){$("header ul li").animate({lineHeight:t+"px"},5)})})}else if(e>0&&e<100){$("header").animate({height:"50px"},5,function(){$("h1.home").animate({lineHeight:"50px"},5,function(){$("header ul li").animate({lineHeight:"50px"},5)})})}else{$("header").animate({height:"40px"},5,function(){$("h1.home").animate({lineHeight:"40px"},5,function(){$("header ul li").animate({lineHeight:"40px"},5)})})}}})
+$(window).scroll(function () { 
+	if($(window).width() > 850) {
+		var offset = window.pageYOffset;
+			if(offset <= 0) {
+				var newHeight = 50 + (offset * -1);
+				$("header").animate({ height: newHeight }, 5, function() {
+					$("h1.home").animate({ lineHeight: newHeight + "px" }, 5, function() {
+						$("header ul li").animate({ lineHeight: newHeight + "px"}, 5);
+					});
+				});
+	  		}
+			else if(offset > 0 && offset < 100) {
+				$("header").animate({ height: "50px" }, 5, function() {
+					$("h1.home").animate({ lineHeight: "50px" }, 5, function() {
+						$("header ul li").animate({ lineHeight: "50px"}, 5);
+					});
+				});
+			}
+			else {
+				$("header").animate({ height: "40px" }, 5, function() {
+					$("h1.home").animate({ lineHeight: "40px" }, 5, function() {
+						$("header ul li").animate({ lineHeight: "40px"}, 5);
+					});
+				});
+			}
+	}
+});
+
+$("#show-hide-navigation").on("click",
+	function() {
+		$("body").toggleClass("show-nav");
+	})
